8cd8fa4078b2fb2cefc6dc1842334cb454ffc73a
Camila Haas Primieri <camilaprimieri@gmail.com>
2018-06-26 01:55:38 -0300

    com o Let

---
+++ _build/t1.ml
@@ -0,0 +1,138 @@
+(*
+  UNIVERSIDADE FEDERAL DO RIO GRANDE DO SUL
+  INSTITUTO DE INFORMÁTICA
+  DEPARTAMENTO DE INFORMÁTICA TEÓRICA
+  INF05516 - Semântica Formal
+  Prof. Dr. Alvaro Moreira
+
+    Camila Primieri - 172662
+    Pietra Freitas - 242285
+
+    --------------------------------------------
+
+    Avaliador da linguagem L1 com listas e exceções de acordo com a semântica operacional de L1
+*)
+
+
+(*
+  LINGUAGEM L1
+*)
+
+type variable = string;;
+
+
+type operator = Sum | Diff | Mult | Div | Eq | And | Or
+;;
+
+type tipo = TyX of string
+            | TyInt 
+            | TyBool 
+            | TyFn of tipo * tipo 
+            | TyList of tipo
+and 
+     typeEnv = (variable * tipo) list
+;;
+
+
+type expr = Num of int 
+          | Bool of bool 
+          | Bop of operator * expr * expr
+          | Not of expr
+          | If of expr * expr * expr 
+          | Var of variable 
+          | App of expr * expr 
+          | Lam of variable * tipo * expr 
+          | Let of variable * tipo * expr * expr
+          | Lrec of variable * tipo * tipo * variable * tipo * expr * expr
+          | Nil
+          | List of expr * expr
+          | Isempty of expr
+          | Hd of expr
+          | Tl of expr
+          | Raise
+          | Try of expr * expr
+;;
+
+
+type value = Vnum of int 
+           | Vbool of bool 
+           | Vclos of variable * expr * env
+           | Vrclos of variable * variable * expr * env
+and  
+     env = (variable * value) list
+;;
+
+
+(*
+  AMBIENTE
+*)
+
+(* Inclui variáveis novas no ambiente de tipos *)
+let updateEnv variable tipo environment : typeEnv = match environment with
+  |[] -> [(variable, tipo)]
+  | hd::tl -> List.append [(variable, tipo)] environment
+;;
+
+(* Procura uma variável específica no ambiente. Se não achar, retorna o tipo de Raise *)
+let rec searchEnv variable environment : tipo = match environment with
+  | [] -> TyX("not_found")
+  | (k, v)::tl ->
+    if (k = variable)
+    then v
+    else searchEnv variable tl
+;;
+
+(* Ambiente de tipos vazio *)
+let emptyEnv : typeEnv = []
+;;
+
+
+(* 
+  FUNÇÃO COLLECTTYEQS
+*)
+
+let rec collectTyEqs (environment:typeEnv) (e:expr) = (TyInt, [])
+
+;;
+
+(* 
+  FUNÇÃO UNIFY
+*)
+
+let unify typeEqSet = TyInt
+
+;;
+
+(* 
+  FUNÇÃO APPLYSUBS
+*)
+
+let applySubs sigma ty = TyBool
+
+;;
+
+
+(* 
+  ALGORITMO TYPEINFER
+*)
+
+(* Recebe o ambiente de tipos e o programa para ser testado *)
+let typeInfer (environment:typeEnv) (program:expr) : tipo =
+
+  (* A função unify retorna um substituição sigma, que é um mapeamento de variáveis de tipo para tipos 
+     Pode falhar caso o conjunto não tenha solução, porque o programa é mal tipado *)
+  sigma = unify []
+
+  (* A função applySubs aplica a essa substituição ao tipo retornado por collectTyEqs e retorna o tipo final da expressão *)
+  applySubs sigma TyInt
+
+;;
+
+(*
+  TESTES
+*)
+
+(* Limpa o ambiente *)
+let environment = emptyEnv;;
+
+typeInfer environment Not(Bool(true));;
