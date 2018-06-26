bd4e31a860fd381a6f338b317beb0f1590d78071
Camila Haas Primieri <camilaprimieri@gmail.com>
2018-06-25 21:02:29 -0300

    dando erro de sintaxe

--- evaluator.ml
+++ evaluator.ml
@@ -18,13 +18,20 @@
   LINGUAGEM L1
 *)
 
-type variable = string
+type variable = string;;
 
 
-type operator = Sum | Diff | Mult | Div | Eq | And | Or | Not
+type operator = Sum | Diff | Mult | Div | Eq | And | Or | Not;;
 
 
-type tipo  = TyVar of string | TyInt | TyBool | TyFn of tipo * tipo | TyList of tipo
+type tipo = TyVar of string 
+            | TyInt 
+            | TyBool 
+            | TyFn of tipo * tipo 
+            | TyList of tipo
+and 
+     typeEnv = (variable * tipo) list
+;;
 
 
 type expr = Num of int 
@@ -43,6 +50,7 @@ type expr = Num of int
           | Tl of expr
           | Raise
           | Try of expr * expr
+;;
 
 
 type value = Vnum of int 
@@ -51,16 +59,18 @@ type value = Vnum of int
            | Vrclos of variable * variable * expr * env
 and  
      env = (variable * value) list
+;;
 
 
 (*
   AMBIENTE
 *)
 
-(* Inclui variáveis novas no ambiente *)
-let updateEnv variable tipo environment : env = match environment with
+(* Inclui variáveis novas no ambiente de tipos *)
+let updateEnv variable tipo environment : typeEnv = match environment with
   |[] -> [(variable, tipo)]
   | hd::tl -> List.append [(variable, tipo)] environment
+;;
 
 (* Procura uma variável específica no ambiente. Se não achar, retorna Raise *)
 let rec searchEnv variable environment : tipo = match environment with
@@ -69,40 +79,61 @@ let rec searchEnv variable environment : tipo = match environment with
     if (k = variable)
     then v
     else searchEnv variable tl
+;;
 
-let emptyEnv : env = []
+(* Ambiente de tipos vazio *)
+let emptyEnv : typeEnv = []
+;;
 
 
 (* 
   FUNÇÃO COLLECTTYEQS
 *)
 
+let rec collectTyEqs (environment:typeEnv) (e:expr) = (TyInt, [])
+
+;;
 
 (* 
   FUNÇÃO UNIFY
 *)
 
+let unify typeEqSet = TyInt
+
+;;
 
 (* 
   FUNÇÃO APPLYSUBS
 *)
 
+let applySubs sigma ty = TyBool
+
+;;
 
 (* 
   ALGORITMO TYPEINFER
 *)
 
 (* Recebe o ambiente de tipos e o programa para ser testado *)
-typeinfer(typeEnv, program) =
+let typeInfer (environment:typeEnv) (program:expr) : tipo =
 
-    let
-        (* A função collectTyEqs retorna um tipo (ou variável de tipo) e um conjunto de equações de tipo *)
-        (ty, typeEqSet) = collectTyEqs(typeEnv, program)
+  (* A função collectTyEqs retorna um tipo (ou variável de tipo) e um conjunto de equações de tipo *)
+  let (ty, typeEqSet) = collectTyEqs typeEnv program
 
-        (* A função unify retorna um substituição sigma, que é um mapeamento de variáveis de tipo para tipos 
-           Pode falhar caso o conjunto não tenha solução, porque o programa é mal tipado *)
-        sigma = unify(typeEqSet)
-    in
-        (* A função applySubs aplica a essa substituição ao tipo retornado por collectTyEqs e retorna o tipo final da expressão *)
-        applySubs(sigma, ty)
-end
+  (* A função unify retorna um substituição sigma, que é um mapeamento de variáveis de tipo para tipos 
+     Pode falhar caso o conjunto não tenha solução, porque o programa é mal tipado *)
+  sigma = unify typeEqSet
+
+  (* A função applySubs aplica a essa substituição ao tipo retornado por collectTyEqs e retorna o tipo final da expressão *)
+  applySubs sigma ty
+
+;;
+
+(*
+  TESTES
+*)
+
+(* Limpa o ambiente *)
+let environment = emptyEnv;;
+
+typeInfer environment Not(Bool(true));;
